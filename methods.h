f892720687f58a00dd9a09be07a2bf74d4681736
ant6si <ant6si@snu.ac.kr>
2019-06-09 03:34:16 +0900

    02:26 last_commit... No island!

--- methods.h
+++ methods.h
@@ -8,9 +8,9 @@ using namespace std;
 /// Parameters
 int TIME_LIMIT = 500; //500
 int MAX_NUM; // valid gene length
-int POPULATION_SIZE = 30; //100     //300;170
+int POPULATION_SIZE = 100; //100     //300;170
 //for crossover
-float XOVER_RATIO = 0.14; //0.02
+float XOVER_RATIO = 0.041; //0.02
 // for selection
 float MAX_FITNESS = 2.5;
 float MIN_FITNESS = 1.0;
